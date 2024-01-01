24505eb6af92f2b9319f7e99b5b89e8ebb45e8ef
alireza <alirezazamanidev80@gmail.com>
2024-01-01 10:06:52 -0500

    create user model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoURL } from './config/mongoose.config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(getMongoURL()), AuthModule, UserModule],
 })
 export class AppModule {}
