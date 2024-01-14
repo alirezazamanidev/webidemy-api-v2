834da324fcc040ef0c1c5e324f9470606378d55c
alireza <alirezazamanidev80@gmail.com>
2024-01-14 13:26:18 -0500

    ADD Config multer for upload file

--- src/common/utils/function.ts
+++
@@ -1,6 +0,0 @@
-
-
-export function Sum(num1:number,num2:number):number {
-
-    return num1+num2;
-}
---
+++ src/common/utils/multer.ts
@@ -0,0 +1,24 @@
+import { diskStorage } from 'multer';
+import path from 'path';
+import {existsSync, mkdirSync} from 'fs'
+const createRoute = () => {
+  const year = new Date().getFullYear();
+  const month = new Date().getMonth() + 1;
+  const day = new Date().getDay();
+
+  return `/public/uploads/images/courses/${year}/${month}/${day}`;
+};
+export const ImageStorage = diskStorage({
+  destination: (req, file, cb) => {
+    const dir=createRoute();
+
+    mkdirSync(path.join(process.cwd(), dir), {recursive: true});
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+
+    const filePath = createRoute() + '/' + file.originalname;
+    if (existsSync(filePath)) cb(null, file.originalname);
+    else cb(null, Date.now() + '-' + file.originalname);
+  },
+});
