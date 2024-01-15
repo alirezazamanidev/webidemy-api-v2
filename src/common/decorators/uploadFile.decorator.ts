170a717d4b5e2347f747a0dbb30c16ed06150865
alireza <alirezazamanidev80@gmail.com>
2024-01-15 09:43:26 -0500

    Fix Image filter for  upload file

--- src/common/decorators/uploadFile.decorator.ts
+++ src/common/decorators/uploadFile.decorator.ts
@@ -1,12 +1,21 @@
-import { UseInterceptors, applyDecorators } from "@nestjs/common";
+import { BadRequestException, UseInterceptors, applyDecorators } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { ImageStorage } from "../utils/multer";
+import { extname } from "path";
 
 export const UploadFile = (data: string) => {
     return applyDecorators(
       UseInterceptors(
         FileInterceptor(data, {
           storage: ImageStorage,
+          fileFilter(req, file, cb) {
+            const ext = extname(file.originalname);
+            const mimetypes = [".jpg", ".jpeg", ".png", ".webp", ".gif"];
+            if (mimetypes.includes(ext)) {
+              return cb(null, true);
+            }
+            return cb(new BadRequestException('فرمت فایل ارسال شده از فرمت فایل تصاویر نیست!'),null);
+          },
           
         }),
       ),
